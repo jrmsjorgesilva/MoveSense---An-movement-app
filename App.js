72e1e01744482138ccd5bff5b5a5f19f8d946167
jorge machado <jrms_jorgesilva@hotmail.com>
2022-06-01 13:03:21 -0300

    first react native commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Criando o primeiro app react native na prática!</Text>
       <StatusBar style="auto" />
     </View>
   );
